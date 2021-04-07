87f8c3833bafa4a730547b8ec19105a524b69a7c
ShankuBisai <shankubisai1111@gmail.com>
2021-04-07 14:50:19 +0530

    Added new Testcase for orders

--- tests/orders/test_Order.py
+++ tests/orders/test_Order.py
@@ -17,9 +17,24 @@ def testCreatePaidOrderGuestUser():
     assert getOrderResponse["id"] == responseDB[0]["order_id"]
 
 
+@pytest.mark.orders
+@pytest.mark.regression
+@pytest.mark.parametrize("status",
+                         [
+                             pytest.param("cancelled",marks=[pytest.mark.tcid55,pytest.mark.smoke]),
+                             pytest.param("completed",marks=pytest.mark.tcid56),
+                             pytest.param("on-hold",marks=pytest.mark.tcid57),
 
-def testUpdateOrderStatus():
-    pass
+                         ])
+def testUpdateOrderStatus(status):
+    orders = Orders()
+    createOrdeResponse=orders.createOrder()
+    currentStatus = createOrdeResponse["status"]
+    assert  currentStatus != status, f"current status is already {status} not able to run the testcase"
+    response=orders.updateOrder(createOrdeResponse["id"],status=status)
+    newOrderStatus = orders.getOrderById(createOrdeResponse["id"])
+    assert response["status"]==newOrderStatus["status"]
+
 
 
 
